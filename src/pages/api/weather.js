818e9868aa698fb10bd3e0e3db42c1a27e0d4cb8
Anthony Gullion <agullion3@nait.ca>
2026-04-19 20:33:49 -0600

    Fixed the function

--- src/pages/api/weather.js
+++ src/pages/api/weather.js
@@ -1,5 +1,7 @@
-export const getWeather = async (lat, lon) => {
-  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${OPENWEATHER_API_KEY}&units=metric`)
+const API_KEY = process.env.OPENWEATHER_API_KEY
+
+export const getWeather = async (location) => {
+  const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${API_KEY}&units=metric`)
   const data = await response.json();
   return data;
 }
